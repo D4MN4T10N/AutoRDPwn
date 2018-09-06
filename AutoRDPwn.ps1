5ebed0d64fbeecdfe79d6486068ecce5b2c97f0d
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2018-09-06 11:31:47 +0200

    Update AutoRDPwn.ps1

--- AutoRDPwn.ps1
+++ AutoRDPwn.ps1
@@ -194,5 +194,5 @@ $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 
 Write-Host ""
 Write-Host "Iniciando conexión remota.." -ForegroundColor Blue ; sleep -milliseconds 2500
-Get-Process powershell | sort starttime -Descending | Stop-Process -Force
+Get-Process powershell | sort starttime -Descending | Select -First 1 | Stop-Process -Force
 del %TEMP%\AutoRDPwn.ps1
